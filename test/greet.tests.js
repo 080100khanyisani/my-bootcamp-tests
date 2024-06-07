69a7c05347af19d24f8ff8aded3a728306a224c1
080100khanyisani <khanyisani1010@gmail.com>
2024-06-07 08:34:01 +0200

    updated

--- test/greet.tests.js
+++ test/greet.tests.js
@@ -1,8 +1,11 @@
 // import greet from "../greet"
 
-describe('greet test' , function(){
-    it('greet should return a greeting' , function(){
+describe('greet test function to greet someone' , function(){
+    it('greet should return a greeting to Bob' , function(){
         assert.equal(greet('Bob'), 'Hello, Bob');
+
+    });
+    it('greet should return a greeting to Sam' , function(){
         assert.equal(greet('Sam'), 'Hello, Sam');
 
     });
